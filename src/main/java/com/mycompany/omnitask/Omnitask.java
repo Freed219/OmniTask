c8c12ff981aa22c89688168a9c30f64466cef31a
Edinsson <edinssonfernandogutierrez@gmail.com>
2023-02-10 19:04:57 -0500

    creacion del proyecto con netbean

---
+++ src/main/java/com/mycompany/omnitask/Omnitask.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.omnitask;
+
+/**
+ *
+ * @author edinsson
+ */
+public class Omnitask {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
