3c56894cb80ce7a69c54b0045910c434d54896d1
Freed219 <67379078+Freed219@users.noreply.github.com>
2023-02-11 16:26:05 -0500

    Override

--- build/classes/CTarea.rs
+++
@@ -1,1 +0,0 @@
-com.mycompany.omnitask.CTarea
---
+++ src/MainFXMLController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Jose
+ */
+public class MainFXMLController implements Initializable {
+
+    @FXML
+    private Button AddTask;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void EnterKeyPressed(KeyEvent event) {
+    }
+
+    @FXML
+    private void MouseClicked(MouseEvent event) {
+    }
+    
+}
