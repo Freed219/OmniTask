f390e98e74c9c07d66c0e16d5af3c227a4b25988
Edinsson <edinssonfernandogutierrez@gmail.com>
2023-02-11 21:48:20 -0500

    se agrego el codigo dentro de la clase estado

--- src/modelo/estado.java
+++ src/modelo/estado.java
@@ -9,5 +9,39 @@ package modelo;
  * @author admin
  */
 public class estado {
-    
+    private int id;
+    private String name;
+    private String abreviatura;
+
+    public estado() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    @Override
+    public String toString() {
+        return "estado{" + "id=" + id + ", name=" + name + ", abreviatura=" + abreviatura + '}';
+    }
 }
