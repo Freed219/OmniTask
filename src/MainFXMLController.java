a5a08e307bb2f957d719a3fe6d5d4daff31e43f9
Freed219 <67379078+Freed219@users.noreply.github.com>
2023-02-11 23:48:45 -0500

    Change

--- src/MainFXMLController.java
+++ src/MainFXMLController.java
@@ -3,20 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -26,19 +21,24 @@ import javafx.stage.Stage;
  */
 public class MainFXMLController implements Initializable {
     @FXML
-    Button AddTask;
+    private Button AddTask;
     @FXML
-    private AnchorPane Tareas;
+    private Button Tareas;
     @FXML
     Button Historial;
+    @FXML
+    Button Ayuda;
+    @FXML
+    private Button AcercaDe;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
+    @FXML
     public void changeHistorial() throws IOException
     {
         Parent hist = FXMLLoader.load(getClass().getResource("HistoricalFXML.fxml"));
@@ -48,13 +48,36 @@ public class MainFXMLController implements Initializable {
         window.show();
     }
     
-
     @FXML
-    private void EnterKeyPressed(KeyEvent event) {
+    public void changeTareas() throws IOException
+    {
+        Parent hist = FXMLLoader.load(getClass().getResource("HistoricalFXML.fxml"));
+        Scene histscene = new Scene(hist, 1140, 750);
+        Stage window = (Stage) Historial.getScene().getWindow();
+        window.setScene(histscene);
+        window.show();
     }
-
+    
     @FXML
-    private void MouseClicked(MouseEvent event) {
+    public void changeAyuda() throws IOException
+    {
+        Parent help = FXMLLoader.load(getClass().getResource("HelpFXML.fxml"));
+        Scene helpscene = new Scene(help, 1140, 750);
+        Stage window = (Stage) Ayuda.getScene().getWindow();
+        window.setScene(helpscene);
+        window.show();
     }
+    
+    @FXML
+    public void changeAcercade() throws IOException
+    {
+        Parent hist = FXMLLoader.load(getClass().getResource("HistoricalFXML.fxml"));
+        Scene histscene = new Scene(hist, 1140, 750);
+        Stage window = (Stage) Historial.getScene().getWindow();
+        window.setScene(histscene);
+        window.show();
+    }
+    
+
 
 }
