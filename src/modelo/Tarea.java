7c28cd8740d97b136714e1ce292219f9bcb6ab40
Edinsson <edinssonfernandogutierrez@gmail.com>
2023-02-11 21:06:35 -0500

    clase tarea

--- src/modelo/Tarea.java
+++ src/modelo/Tarea.java
@@ -3,11 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+import java.util.Date;
 /**
  *
  * @author admin
  */
 public class Tarea {
-    
+    private int id;
+    private String Descripcion;
+    private Date fechaIicio;
+    private 
 }
