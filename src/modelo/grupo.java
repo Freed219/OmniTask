6196f565ce1d6b398d112297766dec5bec74a4c5
Edinsson <edinssonfernandogutierrez@gmail.com>
2023-02-11 21:51:42 -0500

    se agrego el codigo de la clase grupo

--- src/modelo/grupo.java
+++ src/modelo/grupo.java
@@ -9,5 +9,39 @@ package modelo;
  * @author admin
  */
 public class grupo {
-    
+    private int id;
+    private String nombre;
+    private String Descripcion;
+
+    public grupo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "grupo{" + "id=" + id + ", nombre=" + nombre + ", Descripcion=" + Descripcion + '}';
+    }
 }
