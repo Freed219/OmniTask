dfd41cb85726c79a7878a69c404d79785af4ce5f
Edinsson <edinssonfernandogutierrez@gmail.com>
2023-02-11 22:13:03 -0500

    se agrego algo de codigo a la clase conexionMYsql

--- src/conexion/ConexionMysql.java
+++ src/conexion/ConexionMysql.java
@@ -3,11 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package conexion;
-
+import java.sql.Connection;
 /**
  *
  * @author admin
  */
 public class ConexionMysql {
-    
+    private Connection connection;
+    private String usuario="root";
+    private String contrasenia="123456";
+    private String servidor="localhost";
+    private String puerto="3306";
+    private String nombreDB=
 }
