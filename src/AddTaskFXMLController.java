576c3657260d059477e4fda5b57b36bced8274d3
Kenshirodroid <jmario160402@gmail.com>
2023-02-12 23:45:55 -0500

    ID FX

--- src/AddTaskFXMLController.java
+++ src/AddTaskFXMLController.java
@@ -5,9 +5,12 @@
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -18,6 +21,18 @@ public class AddTaskFXMLController implements Initializable {
 
     @FXML
     private Button agregarTarea;
+    @FXML
+    private TextField Descripcion;
+    @FXML
+    private DatePicker FechaIncio;
+    @FXML
+    private DatePicker HoraInico;
+    @FXML
+    private DatePicker FechaFin;
+    @FXML
+    private DatePicker HoraFin;
+    @FXML
+    private TextField Dias;
 
     /**
      * Initializes the controller class.
@@ -26,5 +41,8 @@ public class AddTaskFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void ButtonADD(ActionEvent event) {      
+    }   
 }
