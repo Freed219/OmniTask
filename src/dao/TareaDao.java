9ff1dec8488cf052342360609575501226b95952
Edinsson <edinssonfernandogutierrez@gmail.com>
2023-02-12 23:17:50 -0500

    funciona el filtro

--- src/dao/TareaDao.java
+++ src/dao/TareaDao.java
@@ -59,15 +59,15 @@ public class TareaDao {
     public List<Tarea> listarTareas(boolean cumplida){
         List<Tarea> pendientes=new ArrayList<>();//entiendase pendientes como listaTareas
         try {
-            String SQL="SELECT(id,descripcion,fecha_inicio"
-                        + "hora_inicio,fecha_fin,"
+            String SQL="SELECT id "//,descripcion,fecha_inicio,"
+                        /*+ "hora_inicio,fecha_fin,"
                         + "hora_fin,id_grupo,"
-                        + "fecha_registro,id_estado)FROM "
+                        + "fecha_registro,id_estado)*/+"FROM "
                         + "tareas where(cumplida=";
             if(cumplida){//filtrara tareas completadas (historial)
-                SQL=SQL+"True);";
+                SQL=SQL+"1);";
             }else{//filtrara tareas pendientes
-                SQL=SQL+"False);";
+                SQL=SQL+"0);";
             }
             Connection connection=this.fabricaConexion.getConnection();
             PreparedStatement sentencia=connection.prepareStatement(SQL);
@@ -75,48 +75,49 @@ public class TareaDao {
             while(data.next()){
                 Tarea tarea=new Tarea();
                 tarea.setId(data.getInt(1));
-                tarea.setDescripcion(data.getString(2));
+                /*tarea.setDescripcion(data.getString(2));
                 tarea.setFechaIicio(data.getDate(3));
                 tarea.setHoraInicio(data.getTime(4));
                 tarea.setFechaFin(data.getDate(5));
                 tarea.setHoraFin(data.getTime(6));
                 tarea.setIdGrupo(data.getInt(7));
                 tarea.setFechaRegistro(data.getDate(8));
-                tarea.setEstado(data.getInt(9));
+                tarea.setEstado(data.getInt(9));*/
                 pendientes.add(tarea);
             }
             data.close();
             sentencia.close();
+            
 
         } catch (Exception e) {
             System.err.println("Ocurrio un error al listar tareas");
             System.err.println("Mensaje de error: "+e.getMessage());
             System.err.println("Detalle");
             e.printStackTrace();
+
         }
         return pendientes;
     }
     
     public boolean editar(Tarea tarea){
         try {
-            String SQL="update tareas set descripcion=?,"
-                    + "fecha_inicio=?,hora_inicio=?,"
-                    + "fecha_fin=?,hora_fin=?,"
-                    + "id_estado=?,"
-                    + "id_grupo=?,cumplida=?,"
-                    + "fecha_registro=? where id=?";
+            String SQL="UPDATE tareas SET descripcion=?,"
+                    + "fecha_inicio=?, hora_inicio=?,"
+                    + "fecha_fin=?, hora_fin=?,"
+                    + "id_grupo=?, cumplida=? "
+                    + "WHERE id=?;";
             Connection connection=this.fabricaConexion.getConnection();
             PreparedStatement sentencia=connection.prepareStatement(SQL);
             sentencia.setString(1, tarea.getDescripcion());
             sentencia.setDate(2, tarea.getFechaIicio());
             sentencia.setTime(3, tarea.getHoraInicio());
-            sentencia.setDate(3, tarea.getFechaFin());
+            sentencia.setDate(4, tarea.getFechaFin());
             sentencia.setTime(5, tarea.getHoraFin());
-            sentencia.setInt(6,tarea.getEstado());
-            sentencia.setInt(7, tarea.getIdGrupo());
-            sentencia.setInt(8, tarea.isCumplida());
-            sentencia.setDate(9, tarea.getFechaRegistro());
-            sentencia.setInt(10, tarea.getId());
+            //sentencia.setInt(6,tarea.getEstado());
+            sentencia.setInt(6, tarea.getIdGrupo());
+            sentencia.setInt(7, tarea.isCumplida());
+            //sentencia.setDate(9, tarea.getFechaRegistro());
+            sentencia.setInt(8, tarea.getId());
             
             sentencia.executeUpdate();
             sentencia.close();
